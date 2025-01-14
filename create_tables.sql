84aa767a1579b09b5c7e632a05452f5623b5fb6d
Chi Vo <volinhuyenchi@gmail.com>
2025-01-14 13:21:11 -0800

    The queries for creating tables in PostgreSQL.

---
+++ create_tables.sql
@@ -0,0 +1,74 @@
+create table customers (
+    customer_id bigint primary key,
+    first_name varchar(100),
+    last_name varchar(100),
+    customer_type varchar(50)
+);
+
+create table tickets (
+    ticket_id int primary key,
+    type varchar(50),
+    price float,
+    expiry date,
+    customer_id bigint,
+    constraint fk_customer
+        foreign key (customer_id)
+        references customers(customer_id)
+);
+
+create table customer_trips (
+    ctrip_id int primary key,
+    date date,
+    customer_id bigint,
+    constraint fk_customer
+        foreign key (customer_id)
+        references customers(customer_id)
+);
+
+create table metro_stops (
+    stop_id int primary key,
+    stop_name varchar(100)
+);
+
+create table metro_lines (
+    line_id int primary key,
+    stop_quantity int
+);
+
+create table stop_lists (
+    stop_id int,
+    line_id int,
+    primary key (stop_id, line_id),
+    constraint fk_metro_stop
+        foreign key (stop_id)
+        references metro_stops(stop_id),
+    constraint fk_metro_line
+        foreign key (line_id)
+        references metro_lines(line_id)
+);
+
+create table metro_trips (
+    trip_id int primary key,
+    departure_time time,
+    direction int,
+    stop_id int,
+    line_id int,
+    constraint fk_metro_stop
+        foreign key (stop_id)
+        references metro_stops(stop_id),
+    constraint fk_metro_line
+        foreign key (line_id)
+        references metro_lines(line_id)
+);
+
+create table trip_information (
+    ctrip_id int,
+    trip_id int,
+    primary key (ctrip_id, trip_id),
+    constraint fk_customer_trip
+        foreign key (ctrip_id)
+        references customer_trips(ctrip_id),
+    constraint fk_metro_trip
+        foreign key (trip_id)
+        references metro_trips(trip_id)
+);
